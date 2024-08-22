85af5080576c6034fb9a78d31f754c2d94c06b96
Matthewokiemute <ego.okiemute@gmail.com>
2024-08-22 22:52:22 +0100

    twak integration

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,10 @@
-
+'use client'
 import './globals.css';
 import GlobalState from '@/context';
 import Navbar from '@/components/Navbar';
-import Head from 'next/head';
 import WithdrawalNotifications from '@/components/Alerts/WithdrawalNotifications';
 import LanguageTranslate from '@/components/LanguageTranslate';
+import { useEffect } from 'react';
 
 // const inter = Inter({ subsets: ['latin'] });
 
@@ -12,11 +12,27 @@ export const metadata = {
   title: 'Spiketraderfx - Built for investors interested in high yields, stable returns and also high liquidity.',
   description: 'Invest with confidence on worlds leading asset management and investment platform.',
   icons: {
-    icon: '/favicon.ico', // Assuming the favicon is in the public directory
+    icon: '/favicon.ico',
   },
 };
 
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    // Create the script element
+    const script = document.createElement('script');
+    script.src = 'https://embed.tawk.to/66c7b0fb50c10f7a009f7709/1i5u0mm2k';
+    script.async = true;
+    script.charset = 'UTF-8';
+    script.setAttribute('crossorigin', '*');
+
+    // Append the script to the body
+    document.body.appendChild(script);
+
+    // Cleanup to remove the script when the component is unmounted
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
   return (
     <html lang="en">
       <body>
@@ -30,7 +46,7 @@ export default function RootLayout({ children }) {
             {children}
           </main>
           {/* <Footer /> */}
-          
+
         </GlobalState>
         <script src='https://code.jquery.com/jquery-3.4.1.min.js'></script>
         <script
